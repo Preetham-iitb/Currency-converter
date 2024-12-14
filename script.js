4beba15629f8d63320ea35ec64821d311c233936
Ganesh Preetham Vulise <preethamvulise@gmail.com>
2024-12-14 17:53:40 +0530

    Solved the bugs in script.js

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
-// if user enetered any non positive number, then it is changed to 1
+// If user entered any non-positive number, then it is changed to 1
 document.getElementById("amount").addEventListener("blur", function () {
   var value = parseInt(this.value, 10);
   if (value < 1 || value == "") {
     this.value = 1;
   }
 });
-const dropList = document.querySelectorAll("div select"),// a list all "select" occurances inside a div 
-fromCurrency = document.getElementById("amount"),// from currency
-toCurrency = document.getElementById("to-curr"),// to currency
-convertButton = document.getElementById("convert");
+
+const dropList = document.querySelectorAll("div select"); // All select elements inside a div
+const fromCurrency = document.getElementById("from-curr"); // Corrected to from currency select
+const toCurrency = document.getElementById("to-curr"); // To currency select
+const convertButton = document.getElementById("convert"); // Convert button
+
 for (let i = 0; i < dropList.length; i++) {
-    for(let currency_code in country_list){
-        // selecting USD by default as FROM currency and INR as TO currency
-        let selected = i == 0 ? (currency_code == "USD" ? "selected" : "") : (currency_code == "INR" ? "selected" : "");
-        // creating option tag with passing currency_code as value
-        let option = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-        // inserting options tag inside select tag
-        dropList[i].insertAdjacentHTML("beforeend", option);// options are inserted into select element
+  for (let currency_code in country_list) {
+    // Ensure country_list is defined
+    // Selecting USD by default as FROM currency and INR as TO currency
+    let selected = "";
+    if (i == 0) {
+      if (currency_code == "USD") selected = "selected";
     }
+    else {
+      if (currency_code == "INR") selected = "selected";
+    }
+    // Creating option tag with passing currency_code as value
+    let option = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    // Inserting options tag inside select tag
+    dropList[i].insertAdjacentHTML("beforeend", option); // Options are inserted into select element
+  }
 }
 
+convertButton.addEventListener("click", (e) => {
+    exchangeRate();
+});// when the event "click" happens to convertButton, function is executed
 
-convertButton.addEventListener("click", () => {exchangeRate()})//when convert button is clicked, result is shown
+function exchangeRate() { // Fetches API and does calculation part
+  const inp_amt = document.getElementById("amount").value; // Input value
+  const opt_txt = document.getElementById("to-amount"); // Output element (to be decided in the function)
+  opt_txt.innerText = "Calculating..."; // In case fetching API takes time
+  let api_url = `https://v6.exchangerate-api.com/v6/799bbbe0e78f330e935cc924/latest/${fromCurrency.value}`; // URL for API
 
-function exchangeRate(){ // fetches api and does calc part
-    const inp_amt = document.getElementById("amount").value,// input value
-    opt_txt = document.getElementById("to-amount");// output (to be decided in the function)
-    opt_txt.innerText = "Calculating...";// in case fetching api took time
-    let api_url = `https://v6.exchangerate-api.com/v6/d6600c1a9305a62340e80440/latest/${fromCurrency.value}`;// url for api
-    fetch(api_url).then(Response => Response.json()).then(result => { // APIs gives response in JSON format so I first converted it into a JavaScript object
-        let exchange_rate = result.conversion_rates[toCurrency.value], // read the README file for more explanation (in short, name conversion_rates is checked with API-endpoint Doctumentation)
-        finalExRate = (inp_amt * exchange_rate).toFixed(2);// limiting the number of decimal places to 2
-        opt_txt.innerText = `${finalExRate}`;
-    }).catch(() => {opt_txt.innerText = "Unknown error occured :("})//error handling
+  fetch(api_url)
+    .then((response) => response.json())
+    .then((result) => {
+      let exchange_rate = result.conversion_rates[toCurrency.value]; // Getting user selected TO currency rate
+      let finalExRate = (inp_amt * exchange_rate).toFixed(2); // Limiting the number of decimal places to 2
+      opt_txt.innerText = `${finalExRate}`;
+    })
+    .catch((error) => {
+      opt_txt.innerText = "An unknown error occured :(";
+    });
 }
